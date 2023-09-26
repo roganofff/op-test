727062cca958d876f04da7d43e63c63d0315d8ea
SashaSo4i <114587938+SashaSo4i@users.noreply.github.com>
2023-09-26 16:15:19 +0300

    Update main.py

--- workflows/main.py
+++ workflows/main.py
@@ -1,15 +1,32 @@
-"""Summator module."""
+"""Detective problem"""
 
+from typing import List
 
-def summator(fst: int, snd: int) -> int:
+def detective(password: str) -> List[str]:
     """
-    Get the sum of two values.
-
-    Args:
-        fst: int - first value
-        snd: int - second value
-
-    Returns:
-        int - result of summator
+    Get
     """
-    return fst + snd
+    answer = list()
+    
+    varies = {
+        '1': ('2', '4'),
+        '2': ('1', '3', '5'),
+        '3': ('2', '6'),
+        '4': ('1', '5', '7'),
+        '5': ('2', '4', '6', '8'),
+        '6': ('5', '3', '9'),
+        '7': ('4', '8'),
+        '8': ('5', '7', '9'),
+        '9': ('8', '6'),
+        '0': ('8', )
+    }
+
+    for i in range(len(password)):
+        for value in varies.get(password[i]):
+            temp = password
+            for digit in value:
+                answer.append(temp[:i] + digit + temp[i+1:])
+    answer.insert(0, password)
+
+    return answer
+    
