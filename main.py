affff3f3b85bcaff9b08cd2e764b16495af80468
yehor <114526674+roganofff@users.noreply.github.com>
2023-09-26 16:25:30 +0300

    Final changes

---
+++ main.py
@@ -0,0 +1,31 @@
+"""Detective problem"""
+
+from typing import List
+
+
+def detective(password: str) -> List[str]:
+    """
+    Get
+    """
+    answer = list()
+    varies = {
+        '1': ('2', '4'),
+        '2': ('1', '3', '5'),
+        '3': ('2', '6'),
+        '4': ('1', '5', '7'),
+        '5': ('2', '4', '6', '8'),
+        '6': ('5', '3', '9'),
+        '7': ('4', '8'),
+        '8': ('5', '7', '9'),
+        '9': ('8', '6'),
+        '0': ('8', )
+    }
+
+    for i in range(len(password)):
+        for value in varies.get(password[i]):
+            temp = password
+            for digit in value:
+                answer.append(temp[:i] + digit + temp[i + 1:])
+    answer.insert(0, password)
+
+    return answer
---
+++ test_main.py
@@ -0,0 +1,28 @@
+"""A module for testing main detective_password function."""
+
+from typing import Tuple
+
+import pytest
+
+from main import detective
+
+test_data = [
+    ('35', ['35', '25', '65', '32', '34', '36', '38']),
+    ('00', ['00', '80', '08']),
+    ('123', ['123', '223', '423', '113', '133', '153', '122', '126']),
+    ('56', ['56', '26', '46', '66', '86', '55', '53', '59'])
+]
+
+
+@pytest.mark.parametrize('numbers, expected', test_data)
+def test_detective(numbers: Tuple, expected: str) -> None:
+    """Test detective function with test_data.
+
+    Args:
+        numbers: numbers to be passed to detective.
+        expected: an actual expected detective result.
+
+    Asserts:
+        True if detective returns expected answers.
+    """
+    assert detective(numbers) == expected
